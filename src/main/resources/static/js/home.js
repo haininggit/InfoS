8708738d6c4027cb42db85cc607f09dbd9e79359
HJQ <1109429602@qq.com>
2020-06-04 00:08:17 +0800

    主页

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -88,17 +88,18 @@ $(function(){
                     if (result.success)
                         history.go(0);
                     else
-                        alert("result.errMsg");
+                        alert(result.errMsg);
                 },
             })
         }else{
-            var form = new FormData($('#uploadvid')[0]);
+            var form = new FormData();
+            var file=$('#files')[0].files[0];
+            form.append("file",file);
             form.append("messageInfo", messageInfo);
             form.append("userId", userId);
             form.append("lable", lable);
-            console.log(form);
             $.ajax({
-                url: " message/saveMessageOnVideo",
+                url: "message/saveMessageOnVideo",
                 type: 'POST',
                 data: form,
                 async: true,
@@ -109,11 +110,10 @@ $(function(){
                     if (result.success)
                         history.go(0);
                     else{
-                        alert("result.errMsg");
+                        alert(result.errMsg);
                         console.log("hhhh");
                     }
                 },
-
 
             })
         }
@@ -160,12 +160,12 @@ $("#pic").click(function () {
 });
 
 $("#vid").click(function () {
-    document.getElementsByClassName("upload-content-video")[0].style.display="block";
+    document.getElementsByClassName("files")[0].style.display="block";
 });
 
 
 $("#article-classify").click(function () {
-    document.getElementsByClassName("seclctlable")[0].style.display="block";
+    // document.getElementById("seclctlable").style.display="block";
     $.ajax({
         type: "POST",
         url: "lable/getAllLable",
@@ -173,8 +173,8 @@ $("#article-classify").click(function () {
             if (result.success){
                 let parentlable = document.getElementsByClassName("seclctlable")[0];
                 for(let i=0;i<result.data.length;i++){
-                    console.log(result.data[i].lableInfo);
-                    console.log(result.data[i].lableId);
+                    // console.log(result.data[i].lableInfo);
+                    // console.log(result.data[i].lableId);
                     let divlable = document.createElement("li");
                 divlable.className="box";
                 divlable.innerHTML="<div class=\"box\">\n" +
@@ -216,9 +216,9 @@ window.onload = function () {
                         "                                <div class=\"blog-user-info\">\n" +
                         "                                    <!--头像 昵称 发送时间-->\n" +
                         "                                    <div class=\"B-left\">\n" +
-                        "                                        <div class=\"left-avatar\"><img src='" + result.data.user.userImg + "' alt = \"头像\"></div>\n" +
+                        "                                        <div class=\"left-avatar\"><img src='" + result.data.messages[i].user.userImg + "' alt = \"头像\"></div>\n" +
                         "                                        <div class=\"name-time\">\n" +
-                        "                                            <h2 class=\"blog-nickname\">" + result.data.user.userName + "</h2>\n" +
+                        "                                            <h2 class=\"blog-nickname\">" + result.data.messages[i].user.userName + "</h2>\n" +
                         "                                            <h3 class=\"send-time\">" + result.data.messages[i].message.messageCtime + "</h3>\n" +
                         "                                        </div>\n" +
                         "                                    </div>\n" +
@@ -244,9 +244,10 @@ window.onload = function () {
                         "                                <!--评论 点赞等-->\n" +
                         "                                <div class=\"blog-views\">\n" +
                         "                                    <span>阅读&nbsp;<span class=\"read-num\">" + result.data.messages[i].message.messageReadNum + "</span></span>\n" +
-                        "                                    <a href = \"javascript:;\" class=\"forward\">转发" + result.data.messages[i].message.messageReadNum + "</a>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"forward\" onclick='forward(this)'>转发" + result.data.messages[i].message.messageReadNum + "</a>\n" +
                         "                                    <a href = \"javascript:;\" class=\"comment\">评论" + result.data.messages[i].message.messageCommentNum + "</a>\n" +
-                        "                                    <a href = \"javascript:;\" class=\"praise\">点赞" + result.data.messages[i].message.messageAgreeNum + "</a>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"praise\" onclick='agree(this)'>点赞" + result.data.messages[i].message.messageAgreeNum + "</a>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"praise\" onclick='collect(this)'>收藏</a>\n" +
                         "                                </div>\n" +
                         "                            </div>";
 
@@ -278,9 +279,187 @@ window.onload = function () {
 };
 
 
+// 收藏
+function collect(obj) {
+
+    let parentid=obj.parentNode.parentNode.parentNode.id;
+    console.log(parentid);
+    let userId = $.cookie("userId");
+    console.log(userId);
+    $.ajax({
+        type: "POST",
+        url: "collection/addCollection",//改这里
+        data: {
+            userId: userId,
+            messageId:parentid
+        },
+        success: function (result) {
+                  if (result.success){
+                      alert(result.data)
+                  }else {
+                      alert(result.errorMsg)
+                  }
+        },
+    })
+}
+
+//取消收藏
+function uncollect(obj) {
+
+    let messageid=obj.parentNode.parentNode.parentNode.id;
+    // console.log(parentid);
+    let userId = $.cookie("userId");
+    console.log(userId);
+    $.ajax({
+        type: "POST",
+        url: "collection/delCollection",//改这里
+        data: {
+            userId: userId,
+            messageId:messageid
+        },
+        success: function (result) {
+            if (result.success){
+                let uls = document.getElementsByClassName("blogs")[0];
+                let lis=document.getElementById(obj.parentNode.parentNode.parentNode.id);
+                uls.removeChild(lis);
+                // alert(result.data)
+            }else {
+                alert(result.errorMsg)
+            }
+        },
+    })
+}
+
 // 点赞
+function agree(obj) {
+
+    let parentid=obj.parentNode.parentNode.parentNode.id;
+    let userId = $.cookie("userId");
+    $.ajax({
+        type: "POST",
+        url: "agree/addAgree",//改这里
+        data: {
+            userId: userId,
+            messageId:parentid
+        },
+        success: function (result) {
+            if (result.success){
+                var text = obj.innerText;
+                var num = 1 + parseInt(text.match(/\d+/g));
+                obj.innerHTML = "点赞" + num;
+            }else {
+                alert(result.errorMsg)
+            }
+        },
+    })
+}
+
 
 // 转发
+function forward(obj) {
+    let parentid=obj.parentNode.parentNode.parentNode.id;
+    let userId = $.cookie("userId");
+    $.ajax({
+        type: "POST",
+        url: "forward",//改这里
+        data: {
+            userId: userId,
+            messageId:parentid
+        },
+        success: function (result) {
+            if (result.success){
+                history.go(0);
+                alert(result.data)
+            }else {
+                alert(result.errorMsg)
+            }
+        },
+    })
+}
 
-// 收藏
 
+
+$("#collect").click(function () {
+
+    let parentdiv = document.getElementsByClassName("blogs")[0];
+    parentdiv.innerHTML="";
+    let writearticle = document.getElementsByClassName("writearticle")[0];
+    writearticle.innerHTML="";
+    let userId = $.cookie("userId");
+    $.ajax({
+        type: "POST",
+        url: "collection/collectionInfoPage",//改这里
+        data: {
+            userId: userId,
+        },
+        success: function (result) {
+            console.log(result.data);
+            if (result.success) {
+                for (let i = 0; i < result.data.length; i++) {
+                    let article = document.createElement("li");
+                    article.id = result.data[i].messageInfo.message.messageId;
+                    article.innerHTML = "<div class=\"blog-wrapper\">\n" +
+                        "                                <!--个人信息分为左右两部分-->\n" +
+                        "                                <div class=\"blog-user-info\">\n" +
+                        "                                    <!--头像 昵称 发送时间-->\n" +
+                        "                                    <div class=\"B-left\">\n" +
+                        "                                        <div class=\"left-avatar\"><img src='" + result.data[i].user.userImg + "' alt = \"头像\"></div>\n" +
+                        "                                        <div class=\"name-time\">\n" +
+                        "                                            <h2 class=\"blog-nickname\">" + result.data[i].user.userName + "</h2>\n" +
+                        "                                            <h3 class=\"send-time\">" + result.data[i].messageInfo.message.messageCtime + "</h3>\n" +
+                        "                                        </div>\n" +
+                        "                                    </div>\n" +
+                        "                                    <div class=\"B-right\">\n" +
+                        "                                        <a href = \"javascript:;\" class=\"fas-a\"><i class=\"fas fa-angle-down\"></i></a>\n" +
+                        "                                        <!--设置 删除,下拉列表呈现-->\n" +
+                        "                                        <div class=\"appear\">\n" +
+                        "                                            <div class=\"blog-setting\">\n" +
+                        "                                                <a href = \"javascript:;\">删除</a>\n" +
+                        "                                                <a href = \"javascript:;\">置顶</a>\n" +
+                        "                                            </div>\n" +
+                        "                                        </div>\n" +
+                        "                                    </div>\n" +
+                        "                                </div>\n" +
+                        "                                <!--动态正文-->\n" +
+                        "                                <div class=\"blog-content\">\n" +
+                        "                                    <p class=\"blog-text\" onclick='readArticle(this)'>\n" + result.data[i].messageInfo.message.messageInfo + "</p>\n" +
+                        "                                    <!-- 可能动态中只有文字 -->\n" +
+                        "                                    <div class='blog-img-video' >\n" +
+                        "                                    </div>\n" +
+                        "                                </div>\n" +
+                        "                                <!--评论 点赞等-->\n" +
+                        "                                <!--评论 点赞等-->\n" +
+                        "                                <div class=\"blog-views\">\n" +
+                        "                                    <span>阅读&nbsp;<span class=\"read-num\">" + result.data[i].messageInfo.message.messageReadNum + "</span></span>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"comment\">评论" + result.data[i].messageInfo.message.messageCommentNum + "</a>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"praise\" onclick='agree(this)'>点赞" + result.data[i].messageInfo.message.messageAgreeNum + "</a>\n" +
+                        "                                    <a href = \"javascript:;\" class=\"praise\" onclick='uncollect(this)'>取消收藏</a>\n" +
+                        "                                </div>\n" +
+                        "                            </div>";
+
+                    parentdiv.appendChild(article);
+                    if (result.data[i].messageInfo.imgs.length != 0) {
+                        var blogimgvideos = document.getElementsByClassName("blog-img-video")[i];
+                        for (let j = 0; j < result.data[i].messageInfo.imgs.length; j++) {
+                            let images = document.createElement("img");
+                            images.id = result.data[i].messageInfo.imgs[j].imgId;
+                            images.className="artimg";
+                            images.src=result.data[i].messageInfo.imgs[j].imgUrl
+                            blogimgvideos.appendChild(images);
+                        }
+                    }
+                    if (result.data[i].messageInfo.video != null) {
+                        let blogimgvideos = document.getElementsByClassName("blog-img-video")[i];
+                        let videos = document.createElement("video");
+                        videos.id = result.data[i].messageInfo.video.videoId;
+                        videos.src = result.data[i].messageInfo.video.videoUrl;
+                        blogimgvideos.appendChild(videos);
+                        videos.setAttribute("controls","true");
+                    }
+                }
+
+            }
+        }
+    })
+
+})
